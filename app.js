c8282d7d9c72e79df4d215232cb4798f58d4c61b
Shunmurama <mura.faily72@gmail.com>
2023-12-21 22:04:50 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+let hello = "Hello WorlD";
+alert(hello);
+
+let int1 = 1;
+alert(int1);
+
+let int2 = -10;
+alert(int2);
+
+let str = "JavaScriptを学ぶ"
+alert(str);
+
+alert(2 + 3);
+alert(3 * 8);
+
+alert("Hello" + "WORLD");
+
+alert(str + str)
---
+++ if.js
@@ -0,0 +1,10 @@
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert("みかんよりもりんごの方が高い");
+} else if(orange == apple){
+  alert("みかんとりんごは同じ値段です");
+} else{
+  alert("みかんよりもりんごの方が安い");
+}
