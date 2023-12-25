cc309bf5f076e309e231dfae2072d2794761661a
Shunmurama <mura.faily72@gmail.com>
2023-12-25 14:59:04 +0000

    ６章

---
+++ jQuery-Lesson/mouse.js
@@ -0,0 +1,22 @@
+// $(function(){
+//   // マウスを合わせた時
+//   $('.box1').mouseover(function(){
+//     $('.box1').css({ 'background-color': '#0000FF'});
+//   });
+//   // マウスを外した時
+//   $('.box1').mouseout(function(){
+//     $('.box1').css({'background-color': '#FF0000'});
+//   });
+// });
+
+$(function(){
+  // クリックした時
+  $('.box1').on('click',function(){
+    $('.box1').addClass('box1-ext');
+  });
+  // マウスを外した時
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
+  });
+});
+
