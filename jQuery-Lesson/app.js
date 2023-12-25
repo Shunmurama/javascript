82d3f2022812266f188550fddf0115ce72bfa473
Shunmurama <mura.faily72@gmail.com>
2023-12-25 14:19:22 +0000

    5章

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,3 +1,21 @@
-$(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>');
+// $(function() {
+//   $('.box1').css({
+//     'background-color': '#0000FF',
+//     'height': '100px'
+//   });
+// });
+
+// 上から下にアニメーション
+// $(function(){
+//   $('.box1').slideUp(5000);
+// });
+
+// 要素を表示
+// $(function(){
+//   $('.box1').show();
+//   $('.box1').css({'background-color': '#0000FF'});
+// });
+
+$(function(){
+  $('.box1').hide();
 });
